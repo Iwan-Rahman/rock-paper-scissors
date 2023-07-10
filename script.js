615c2e8ad40cc3c3c9c48a6b6a88050070a912dd
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-09 20:36:27 -0400

    Implement Computer Selection

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 console.log("Hello World");
 
+function getComputerChoice(){
+    //Random number from 1-3 where,
+    //Rock = 1, Paper = 2, Scissors = 3
+
+    let choice = Math.floor(Math.random()*3 + 1);
+
+    switch(choice){
+        case 1:
+            choice = "Rock";
+            break;
+        case 2:
+            choice = "Paper";
+            break;
+        case 3:
+            choice = "Scissors";
+            break;
+    }
+
+    return choice;
+}
