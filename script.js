8bdb01b3efb363e3f48eb34e18a774885e27538e
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-11 10:53:51 -0400

    Implement different computer behaviors
    Created three different modes, simple, normal, and custom.
    The user can set how the computer behaves using the different modes,
    and utilizing the comp behavior function. Changed the getComputerChoice
    function to work with this new feature.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let playerScore = 0;
 let compScore = 0;
 
-function getComputerChoice(){
-    //Random number from 1-3 where,
-    //Rock = 1, Paper = 2, Scissors = 3
+function getComputerChoice(rock = rockPriority, paper = paperPriority, scissors = scissorsPriority){
+    //each number sets the priority for that move. At defaut, every move has a 33.33% chance
+    //Each move should have a probability of priority/total for being made. 
 
-    let choice = Math.floor(Math.random()*3 + 1);
+    let total = rock + paper + scissors;
+    let choice = Math.floor(Math.random()*total + 1);
 
-    switch(choice){
-        case 1:
-            choice = "Rock";
-            break;
-        case 2:
-            choice = "Paper";
-            break;
-        case 3:
-            choice = "Scissors";
-            break;
+    //The three cases seperates the total over three intervals. 1 to rock, 
+    //rock to (total - scissors), and  total - scissors to total
+    if(choice <= total - (paper + scissors)){
+        choice = "Rock";
+    }else if(choice > rock && choice <= total - scissors){
+        choice = "Paper";
+    }else {
+        choice = "Scissors";
     }
-
+    
+    console.log(choice);
     return choice;
 }
 
@@ -46,6 +46,7 @@ function updateScore(){
 function updateScreen(computerSelection,playerSelection){
     //all fine names are just lower case of the selection string values
     let playerImg = playerSelection.toLowerCase();
+    console.log(computerSelection);
     let compImg = computerSelection.toLowerCase();
 
     playerDisplayImg.setAttribute('src','./img/'+ playerImg +'.png');
@@ -76,6 +77,12 @@ function playRound(computerSelection,playerSelection){
     return result;
 }
 
+function compBehavior(rock,paper,scissors){
+    rockPriority = rock;
+    paperPriority = paper;
+    scissorsPriority = scissors;
+}
+
 //Make buttons
 const rock = document.querySelectorAll(".player-btn")[0];
 const paper = document.querySelectorAll(".player-btn")[1];
@@ -83,6 +90,32 @@ const scissors = document.querySelectorAll(".player-btn")[2];
 const playerDisplayImg = document.querySelector('.player-display img');
 const compDisplayImg = document.querySelector('.comp-display img');
 
+const simple = document.querySelectorAll(".comp-btn")[0];
+const normal = document.querySelectorAll(".comp-btn")[1];
+const custom = document.querySelectorAll(".comp-btn")[2];
+
+let rockPriority = 1;
+let paperPriority = 1;
+let scissorsPriority = 1;
+
 rock.addEventListener("click",() => gameResult.textContent = playRound(getComputerChoice(),"Rock"));
 paper.addEventListener("click",() => gameResult.textContent = playRound(getComputerChoice(),"Paper"));
 scissors.addEventListener("click", () => gameResult.textContent = playRound(getComputerChoice(),"Scissors"));
+
+simple.addEventListener("click", () => compBehavior(8,1,1));
+normal.addEventListener("click", () => compBehavior(1,1,1));
+custom.addEventListener("click",() => {
+    let rock = 1;
+    let paper = 1;
+    let scissors = 1;
+    do{
+        rock = +prompt("How ofthen should the AI play rock. Set a positive integer from 1 - 10")
+    }while(!(rock >= 1 && rock <= 10) && !(typeof rock != 'number'));
+    do{
+        paper = +prompt("How ofthen should the AI play paper. Set a positive integer from 1 - 10")
+    }while(!(paper >= 1 && paper <= 10) && !(typeof paper != 'number'));
+    do{
+        scissors = +prompt("How ofthen should the AI play scissors. Set a positive integer from 1 - 10")
+    }while(!(scissors >= 1 && scissors <= 10) && !(typeof scissors != 'number'));
+    compBehavior(rock,paper,scissors);
+})
