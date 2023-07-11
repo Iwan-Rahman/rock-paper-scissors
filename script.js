cee875842f5fb7190cc16f53332d5bb1e0cbf169
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-10 22:57:42 -0400

    Remove play game function

--- script.js
+++ script.js
@@ -52,31 +52,6 @@ function playRound(computerSelection,playerSelection){
     return result;
 }
 
-function playGame(rounds = 5){
-    let playerScore = 0;
-    let compScore = 0;
-    for(let i = 0; i < rounds; i++){
-        let result = playRound(getComputerChoice(),getPlayerChoice());
-        if(result == "You win"){
-            playerScore++;
-        }else if(result == "You lose"){
-            compScore++;
-        }
-        console.log(result);
-    }
-
-    console.log("Player Score: " + playerScore);
-    console.log("Computer Score: " + compScore);
-
-    if(playerScore > compScore){
-        console.log("You win the game!");
-    }else if(playerScore == compScore){
-        console.log("It's a tie game!");
-    }else {
-        console.log("You lose the game!");
-    }
-}
-
 //Make buttons
 const rock = document.querySelectorAll(".player-btn")[0];
 const paper = document.querySelectorAll(".player-btn")[1];
